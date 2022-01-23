3c6cb92e5813b9bfaf3c830ba537af13fd39868d
Marijn Suijten <marijn.suijten@somainline.org>
2022-01-23 23:16:49 +0100

    Delete outdated libhardware bootctrl module for unused 1.0 default HAL
    Starting with [device-sony-common#880] the Sony Open Devices project is
    switching all its devices to the newer, updated CAF bootctrl HAL
    implementation, which is provided at version 1.1 and required for FCM5.
    This BluePrint-only file was previously "configuring" a libhardware
    module on a specific version, for use with the 1.0 default HAL provided
    by AOSP.  Their 1.1 default implementation reads and writes boot control
    through a different library which is incompatible with our platforms.
    
    [device-sony-common#880]: https://github.com/sonyxperiadev/device-sony-common/pull/880
    
    Signed-off-by: Marijn Suijten <marijn.suijten@somainline.org>

--- platform.mk
+++ platform.mk
@@ -109,9 +109,7 @@ PRODUCT_PACKAGES += \
     update_engine \
     update_engine_client \
     update_engine_sideload \
-    update_verifier \
-    bootctrl.sm8350 \
-    bootctrl.sm8350.recovery
+    update_verifier
 
 AB_OTA_PARTITIONS += \
     boot \
