084715866c6b3629a46810bd706bd584264a057f
Alin Jerpelea <alin.jerpelea@sony.com>
2022-09-07 14:07:23 +0200

    platform: declare the ssusb property
    on sagami platform USB controller needs the ssusb mode to be set
    
    Signed-off-by: Alin Jerpelea <alin.jerpelea@sony.com>

--- platform.mk
+++ platform.mk
@@ -370,6 +370,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
 
 # USB controller setup
 PRODUCT_PROPERTY_OVERRIDES += \
+    ro.boot.usb.dwc3=a600000.ssusb \
     sys.usb.controller=a600000.dwc3 \
     sys.usb.rndis.func.name=gsi
 
